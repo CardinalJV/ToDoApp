8f67ddc2af47fc6b79c00da81d6c70fb1682dbdb
CardinalJV <jessy.viranaiken@icloud.com>
2024-07-30 11:28:08 +0200

    MaJ de la view

--- ToDoApp/Views/AddNewTaskView.swift
+++ ToDoApp/Views/AddNewTaskView.swift
@@ -9,39 +9,57 @@ import SwiftUI
 
 struct AddNewTaskView: View {
   
+  var targetList: ListModel
+  
   @State var name = ""
   @State var priority = ""
   @State var task_vm = TaskViewModel()
-  var targetList: ListModel
+  @State private var isActive_alert = false
+  
+  @Binding var isPresented: Bool
+  
+  func createTask() {
+      // Vérifie que les champs ne sont pas vides
+    if name.isEmpty {
+      isActive_alert.toggle()
+    } else {
+      Task{
+        await task_vm.createTask(name: self.name, priority: self.priority, lists: [targetList.id])
+      }
+    }
+  }
   
   var body: some View {
-    VStack{
-      List{
-        TextField("Ajouter un nom", text: $name)
-        TextField("Priority entre 1 et 3", text: $priority)
-      }
-      Button(action: {
-        Task{
-          await task_vm.createTask(name: self.name, priority: self.priority, lists: [targetList.id])
+    NavigationStack{
+      VStack{
+        List{
+          TextField("Ajouter un nom", text: $name)
+          TextField("Priority entre 1 et 3", text: $priority)
         }
-      }, label: {
-        Text("Add New Todo")
-      })
-    }
-    .toolbar{
-      ToolbarItem(placement: .topBarLeading) {
-        Text("Annuler")
       }
-        //        ToolbarItem(placement: .topBarLeading) {
-        //          Text("Nouveau Rappel")
-        //        }
-      ToolbarItem(placement: .topBarTrailing) {
-        Text("Ajouter")
+      .navigationTitle("Nouveau rappel")
+      .alert("Champ du formulaire invalide", isPresented: $isActive_alert){}
+      .toolbar{
+        ToolbarItem(placement: .topBarLeading) {
+          Button(action: {
+            isPresented = false
+          }) {
+            Text("Annuler")
+          }
+        }
+        ToolbarItem(placement: .topBarTrailing) {
+          Button(action: {
+            createTask()
+            isPresented = false
+          }) {
+            Text("Ajouter")
+          }
+        }
       }
     }
   }
 }
 
-//#Preview {
-//  AddNewTaskView()
-//}
+  //#Preview {
+  //  AddNewTaskView()
+  //}
