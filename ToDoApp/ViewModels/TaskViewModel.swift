a273d79b0cb211329f15e2efb6eddc510f250143
CardinalJV <jessy.viranaiken@icloud.com>
2024-08-06 17:44:22 +0200

    Create: Ajout de la fonction delete dans le CRUD

--- ToDoApp/ViewModels/TaskViewModel.swift
+++ ToDoApp/ViewModels/TaskViewModel.swift
@@ -11,17 +11,17 @@ import Observation
 @Observable
 class TaskViewModel {
   
-   var tasks = [TaskModel]()
-  var isLoading = false
+  var tasks = [TaskModel]()
+  var isLoading = true
   
   private let apiUrl = "https://api.airtable.com/v0/app3Dfn6h8N2Wzzty/Tasks"
   private let apiToken = "patYRbCYvSI0gxfgE.1cf151356d8b06aa3dca4e81334401120accecbc5b7fac6518606be1d6132291"
   
-  // Create
+    // Create
   func createTask(name: String, priority: String, lists: [String], notes: String? = nil, dateAndHourToNotify: String?) async {
     
     let url = URL(string: apiUrl)!
-    
+    // Création de l'objet JSON qui sera envoyer dans la requete
     let jsonObject: [String: Any] = [
       "fields": [
         "name": name,
@@ -51,21 +51,21 @@ class TaskViewModel {
       
       task.resume()
       
+      self.isLoading = false
+      
     } catch {
       
       print(error.localizedDescription)
       
     }
   }
-  // Read
+    // Read
   func readTasks() async {
     
     let url = URL(string: apiUrl)!
     
     var request = URLRequest(url: url)
     request.setValue("Bearer \(apiToken)", forHTTPHeaderField: "Authorization")
-    
-    self.isLoading = true
     
     do {
       
@@ -81,7 +81,40 @@ class TaskViewModel {
       
       print(error.localizedDescription)
       
+      self.isLoading.toggle()
+      
+    }
+  }
+    // Delete
+  func deleteTask(id: String) async {
+    
+    let url = URL(string: apiUrl + "/" + id)!
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "DELETE"
+    request.setValue("Bearer \(apiToken)", forHTTPHeaderField: "Authorization")
+    
+    do {
+      
+      let (_, response) = try await URLSession.shared.data(for: request)
+        // Déballage de l'optionnal afin de vérifier l'existence de la réponse
+      if let httpResponse = response as? HTTPURLResponse {
+          // Si la requète réussie, statut 200, alors on supprime la tâche localement
+        if httpResponse.statusCode == 200 {
+          print("Suppression réussie")
+          if let index = tasks.firstIndex(where: { $0.id == id }) {
+            tasks.remove(at: index)
+          }
+        } else {
+          print("Failed to delete task: \(httpResponse.statusCode)")
+        }
+      }
+      
       self.isLoading = false
+      
+    } catch {
+      
+      print(error.localizedDescription)
       
     }
   }
