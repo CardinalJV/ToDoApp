bf0c323dbb05f8a0b462da3ea4a09dd1262a1bc3
CardinalJV <jessy.viranaiken@icloud.com>
2024-08-16 18:06:25 +0200

    /Cleaning

--- ToDoApp/ViewModels/ListViewModel.swift
+++ ToDoApp/ViewModels/ListViewModel.swift
@@ -14,9 +14,6 @@ import Foundation
   private let apiUrl = "https://api.airtable.com/v0/app3Dfn6h8N2Wzzty/Lists"
   private let apiToken = "patYRbCYvSI0gxfgE.1cf151356d8b06aa3dca4e81334401120accecbc5b7fac6518606be1d6132291"
   
-//  private let apiUrl = "\(KeyConstant.APIKeys.apiUrl)Lists"
-//  private let apiToken = KeyConstant.APIKeys.apiToken
-  
   // Create
   func createList(title: String, pictureColor: String) async {
 
@@ -80,6 +77,7 @@ import Foundation
       
     }
   }
+  
   // Delete
   func deleteList(id: String) async {
     
