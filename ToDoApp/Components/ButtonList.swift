41ac606cd64bfab74260ed219109de5d031534ba
CardinalJV <jessy.viranaiken@icloud.com>
2024-08-19 19:21:41 +0200

    Delete: Suppression des @State afin d'éviter d'empecher la mise a jour des variables et modification du SF symbole

--- ToDoApp/Components/ButtonList.swift
+++ ToDoApp/Components/ButtonList.swift
@@ -4,21 +4,19 @@
 //
 //  Created by Jessy Viranaiken on 22/07/2024.
 //
-
-import Foundation
 import SwiftUI
 
 struct ButtonList: View {
   
-  @State var title: String
-  @State var itemCount: Int
-  @State var picture: String
-  @State var pictureColor: String
+  var title: String
+  var itemCount: Int
+  var picture: String
+  var pictureColor: String
   
   var body: some View {
     VStack(alignment: .leading){
       HStack{
-        Image(systemName: "calendar.circle.fill")
+        Image(systemName: "list.bullet.circle.fill")
           .font(.system(size: 35))
           .foregroundStyle(ColorsModel().colorFromString(self.pictureColor))
         Spacer()
