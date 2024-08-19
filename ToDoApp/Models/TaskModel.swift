b548cc12713e5a3fadf07577272578b9575ccdb0
CardinalJV <jessy.viranaiken@icloud.com>
2024-08-19 19:24:23 +0200

    Create: Ajout de la fonction formatedDateAndTime

--- ToDoApp/Models/TaskModel.swift
+++ ToDoApp/Models/TaskModel.swift
@@ -24,4 +24,28 @@ struct TaskModelFields: Codable {
   var priority: String? = nil
   var isCompleted: Bool? = nil
   var notes: String? = nil
+  
+  func formattedDateAndTime(dateToNotify: String? = nil) -> String? {
+    let dateString = dateToNotify ?? self.dateToNotify
+    
+    guard let iso8601String = dateString else {
+      return nil
+    }
+    
+      // Convertir l'ISO 8601 en Date
+    let inputFormatter = DateFormatter()
+    inputFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    inputFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+    
+    guard let date = inputFormatter.date(from: iso8601String) else {
+      return nil
+    }
+    
+      // Convertir la Date en chaîne formatée
+    let outputFormatter = DateFormatter()
+    outputFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+    outputFormatter.timeZone = TimeZone.current
+    
+    return outputFormatter.string(from: date)
+  }
 }
